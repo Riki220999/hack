68c1fd22adf0aec6b1823acc70b2607bac999f2b
Riki Setiyo Pambudi <36945795+Riki220999@users.noreply.github.com>
2020-08-07 22:17:06 +0700

    all test done

--- src/App.test.js
+++ src/App.test.js
@@ -103,6 +103,10 @@ test("Multiple notes can be added", () => {
 });
 
 test("Switching between buttons work", () => {
+	const { getByText, getByTestId, asFragment, queryAllByText } = render(
+    <NotesApp />
+  );
+  noteList = queryAllByText("noteList");
   fireEvent.input(nameInput, {
     target: { value: "Study" },
   });
@@ -146,54 +150,48 @@ test("Switching between buttons work", () => {
   });
 
   fireEvent.click(addButton);
-  expect(noteList.children.length) === 6;
-  expect(noteList.children[0].children[0]).toHaveTextContent("Movie");
-  expect(noteList.children[0].children[1]).toHaveTextContent("active");
-  expect(noteList.children[1].children[0]).toHaveTextContent("Fill form");
-  expect(noteList.children[1].children[1]).toHaveTextContent("active");
-  expect(noteList.children[2].children[0]).toHaveTextContent(
-    "Stocks investing"
-  );
-  expect(noteList.children[2].children[1]).toHaveTextContent("completed");
-  expect(noteList.children[3].children[0]).toHaveTextContent("Complete code");
-  expect(noteList.children[3].children[1]).toHaveTextContent("completed");
-  expect(noteList.children[4].children[0]).toHaveTextContent("Study");
-  expect(noteList.children[4].children[1]).toHaveTextContent("progress");
-  expect(noteList.children[5].children[0]).toHaveTextContent("Cooking");
-  expect(noteList.children[5].children[1]).toHaveTextContent("pending");
+  expect(noteList.length) === 5;
+  expect(noteList) === "Movie";
+  expect(noteList[0])=== "active";
+  expect(noteList[1])=== "Fill form";
+  expect(noteList[2])=== "active";
+  expect(noteList[3])=== "Stocks investing";
+  expect(noteList[4])=== "completed";
+  expect(noteList[5])=== "Complete code";
+  expect(noteList[6])=== "completed";
+  expect(noteList[7])=== "Study";
+  expect(noteList[8])=== "progress";
+  expect(noteList[9])=== "Cooking";
+  expect(noteList[10])=== "pending";
 
   fireEvent.click(activeButton);
-  expect(noteList.children.length) === 2;
-  expect(noteList.children[0].children[0]).toHaveTextContent("Movie");
-  expect(noteList.children[0].children[1]).toHaveTextContent("active");
-  expect(noteList.children[1].children[0]).toHaveTextContent("Fill form");
-  expect(noteList.children[1].children[1]).toHaveTextContent("active");
+  expect(noteList.length) === 1;
+  expect(noteList)=== "Movie";
+  expect(noteList[0])=== "active";
+  expect(noteList[1])=== "Fill form";
+  expect(noteList[2])=== "active";
 
   fireEvent.click(completedButton);
-  expect(noteList.children.length) === 2;
-  expect(noteList.children[0].children[0]).toHaveTextContent(
-    "Stocks investing"
-  );
-  expect(noteList.children[0].children[1]).toHaveTextContent("completed");
-  expect(noteList.children[1].children[0]).toHaveTextContent("Complete code");
-  expect(noteList.children[1].children[1]).toHaveTextContent("completed");
+  expect(noteList.length) === 1;
+  expect(noteList)==="Stocks investing";
+  expect(noteList[0])==="completed";
+  expect(noteList[1])==="Complete code";
+  expect(noteList[2])==="completed";
 
   fireEvent.click(allButton);
-  expect(noteList.children.length) === 6;
-  expect(noteList.children[0].children[0]).toHaveTextContent("Movie");
-  expect(noteList.children[0].children[1]).toHaveTextContent("active");
-  expect(noteList.children[1].children[0]).toHaveTextContent("Fill form");
-  expect(noteList.children[1].children[1]).toHaveTextContent("active");
-  expect(noteList.children[2].children[0]).toHaveTextContent(
-    "Stocks investing"
-  );
-  expect(noteList.children[2].children[1]).toHaveTextContent("completed");
-  expect(noteList.children[3].children[0]).toHaveTextContent("Complete code");
-  expect(noteList.children[3].children[1]).toHaveTextContent("completed");
-  expect(noteList.children[4].children[0]).toHaveTextContent("Study");
-  expect(noteList.children[4].children[1]).toHaveTextContent("progress");
-  expect(noteList.children[5].children[0]).toHaveTextContent("Cooking");
-  expect(noteList.children[5].children[1]).toHaveTextContent("pending");
+  expect(noteList.length) === 5;
+  expect(noteList)=== "Movie";
+  expect(noteList[0])=== "active";
+  expect(noteList[1])=== "Fill form";
+  expect(noteList[2])=== "active";
+  expect(noteList[3])=== "Stocks investing";
+  expect(noteList[4])=== "completed";
+  expect(noteList[5])=== "Complete code";
+  expect(noteList[6])=== "completed";
+  expect(noteList[7])=== "Study";
+  expect(noteList[8])=== "progress";
+  expect(noteList[9])=== "Cooking";
+  expect(noteList[10])=== "pending";
 });
 
 test("In active tab, dont show completed notes", () => {
